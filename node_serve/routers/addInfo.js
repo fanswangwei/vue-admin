74f7b8f715390d88429d706fcae99ed641215029
wangwei <15638833667@163.com>
2020-03-10 19:13:27 +0800

    优化

--- node_serve/routers/addInfo.js
+++ node_serve/routers/addInfo.js
@@ -1,7 +1,6 @@
-const Router = require('koa-router'),
-  config = require('../config'),
-  router = new Router(),
-  userInfo = require('../dbs/models/userInfo');
+const Router = require('koa-router');
+const router = new Router();
+
 router.get('/', async (ctx, next) => {
   let title = '文件上传';
   await ctx.render('index', { title })
@@ -9,49 +8,54 @@ router.get('/', async (ctx, next) => {
 
 //路由
 router.post('/api/addUserInfo', async (ctx, next) => {
-  var param = ctx.req;
-  console.log(ctx.req);
-  if (param) {
-    console.error("ERROR: ");
-    ctx.body = {
-      code: 501,
-      data: {
-        message: '缺少参数'
-      }
-    }
-    return false;
-  } else {
-    let params = {
-      name: 'String',
-      age: 'String',
-      sex: 'String',
-      height: 'String',
-      weight: 'String',
-      birth: 'String',
-      phone: 'Number',
-      email: 'String',
-      createdTime: new Date(),
-      remak: 'String'
-    }
+  // var param = ctx.req;
+  console.log(ctx.request.body);
 
-    // 新建数据、保存数据
-    let newData = new userInfo(params)
-    newData.save(function (err) {
-      if (err) {
-        console.log('mongo error: ', err)
-        ctx.body = {
-          code: 500,
-          data: err
-        }
-        return;
-      }
-    });
-    // 获取保存的路径
-    ctx.body = {
-      code: 200,
-      data: config.filePath + params.url // 返回访问路径
-    }
+  ctx.body = {
+    code: 200,
+    data: ctx.request.body
   }
+  // if (param) {
+  //   console.error("ERROR: ");
+  //   ctx.body = {
+  //     code: 501,
+  //     data: {
+  //       message: '缺少参数'
+  //     }
+  //   }
+  //   return false;
+  // } else {
+  //   let params = {
+  //     name: 'String',
+  //     age: 'String',
+  //     sex: 'String',
+  //     height: 'String',
+  //     weight: 'String',
+  //     birth: 'String',
+  //     phone: 'Number',
+  //     email: 'String',
+  //     createdTime: new Date(),
+  //     remak: 'String'
+  //   }
+
+  //   // 新建数据、保存数据
+  //   let newData = new userInfo(params)
+  //   newData.save(function (err) {
+  //     if (err) {
+  //       console.log('mongo error: ', err)
+  //       ctx.body = {
+  //         code: 500,
+  //         data: err
+  //       }
+  //       return;
+  //     }
+  //   });
+  //   // 获取保存的路径
+  //   ctx.body = {
+  //     code: 200,
+  //     data: config.filePath + params.url // 返回访问路径
+  //   }
+  // }
 });
 
 module.exports = router
--- node_serve/serve.js
+++ node_serve/serve.js
@@ -1,27 +1,29 @@
 const Koa = require('koa');
 const Router = require('koa-router');
 const cors = require('koa2-cors');
+const bodyParser = require("koa-bodyparser");
 const {logs} = require('./utils/common');
 const app = new Koa();
 const router = new Router();
 
 //操作数据库,引入模块
-const mongoose = require('mongoose');
-const config = require('./config')
-//mongoose,连接数据库
-mongoose.connect(config.DB_CONN_STR, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-}, function (err, db) {
-    if (err) {
-        console.log('mongo error:', err);
-        return;
-    }
-    console.log("mongo success!");
-})
+// const mongoose = require('mongoose');
+// const config = require('./config')
+// //mongoose,连接数据库
+// mongoose.connect(config.DB_CONN_STR, {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true
+// }, function (err, db) {
+//     if (err) {
+//         console.log('mongo error:', err);
+//         return;
+//     }
+//     console.log("mongo success!");
+// })
 
 // 设置跨域
 app.use(cors())
+app.use(bodyParser())
 app.use(async (ctx, next) => {
     ctx.set('Access-Control-Allow-Origin', '*');
     ctx.set('Access-Control-Allow-Credentials', 'true');
@@ -32,18 +34,6 @@ app.use(async (ctx, next) => {
     } else {
         await next();
     }
-});
-
-
-router.get("/api/health", async (ctx) => {
-    ctx.body = { code: 200 }
-})
-
-app.use(async (ctx) => {
-    var param = ctx.req;
-    logs(ctx, JSON.stringify(ctx.req.body));
-    console.log(param)
-    ctx.body = 'Hello World';
 });
 
 const addUser = require('./routers/addInfo');
